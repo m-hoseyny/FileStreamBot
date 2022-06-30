df011dbfa60d274fcd68579af3504a878453cb6c
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2022-06-30 12:23:04 +0430

    feat: add optional bot token

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -2,11 +2,16 @@
 from pyrogram import Client
 from ..vars import Var
 
+if Var.PHONE_NUMBER:
+    bot_token = None
+else:
+    bot_token = Var.BOT_TOKEN
+
 StreamBot = Client(
     session_name='Web Streamer',
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
-    bot_token=Var.BOT_TOKEN,
+    bot_token=bot_token,
     phone_number=Var.PHONE_NUMBER,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
