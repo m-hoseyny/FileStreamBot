bab60c7445afcca05273135a57047a2ebd3ba777
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2022-06-30 12:19:10 +0430

    feat: change from bot to client

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -7,6 +7,7 @@ StreamBot = Client(
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
+    phone_number=Var.PHONE_NUMBER,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
 )
